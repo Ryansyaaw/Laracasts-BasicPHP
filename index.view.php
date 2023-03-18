8ec314dfec9f1e4b498d56ffcb8d0cc45135c057
= <=>
2023-03-18 09:46:58 +0700

    part10

---
+++ index.view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+</head>
+<body>
+    
+
+    <ul> 
+        <?php foreach($filteredBooks as $book) : ?>
+            <li>
+                <a href="<?= $book['purchaseUrl']?>">
+                <?=$book['name']?>(<?=$book['releaseYear']?>)
+            </a>
+            </li>
+            <?php endforeach?>
+    </ul>
+
+</body>
+</html>
